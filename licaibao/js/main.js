101f429d6a7b1c6d8a3da9f08834ee4478de407b
wangmd0406 <35946399+wangmd0406@users.noreply.github.com>
2018-10-30 20:12:01 +0800

    Add files via upload

---
+++ licaibao/js/main.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel(
+        {
+            items:1,
+            loop:true,
+            autoplay:true,
+            autoplayTimeout:3000,
+            autoplayHoverPause:true
+        }
+    );
+});
